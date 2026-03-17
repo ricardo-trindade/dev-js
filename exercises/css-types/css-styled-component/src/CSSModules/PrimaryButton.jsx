b700e17a21c9b9aa13a451e8eb20670da1d512c7
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-03-17 10:55:15 -0300

    css types

---
+++ exercises/css-types/css-styled-component/src/CSSModules/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import primaryButtonStyles from "./p-btn.module.css"
+
+export default function Button() {
+    return (
+        <button className={primaryButtonStyles.primaryButton} type="button">
+            oi
+        </button>
+    )
+}
---
+++ exercises/css-types/css-styled-component/src/CSSModules/SecondaryButton.jsx
@@ -0,0 +1,9 @@
+import secButtonStyles from "./s-btn.module.css"
+
+export default function Button() {
+    return (
+        <button className={secButtonStyles.primaryButton} type="button">
+            oi
+        </button>
+    )
+}
