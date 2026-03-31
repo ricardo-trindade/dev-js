943f60655dac710194e245387fc8eccf61ce25c6
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-03-31 10:11:07 -0300

    Delete exercises/css-types directory

--- exercises/css-types/css-styled-component/src/CSSModules/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import primaryButtonStyles from "./p-btn.module.css"
-
-export default function Button() {
-    return (
-        <button className={primaryButtonStyles.primaryButton} type="button">
-            oi
-        </button>
-    )
-}
--- exercises/css-types/css-styled-component/src/CSSModules/SecondaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import secButtonStyles from "./s-btn.module.css"
-
-export default function Button() {
-    return (
-        <button className={secButtonStyles.primaryButton} type="button">
-            oi
-        </button>
-    )
-}
